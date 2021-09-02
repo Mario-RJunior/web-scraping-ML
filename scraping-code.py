186d35a328a641c329b2db5d31ed19f3d3c9a3bb
Mario-RJunior <54813020+Mario-RJunior@users.noreply.github.com>
2021-09-02 20:01:14 -0300

    Refatorando o script

--- scraping-code.py
+++ scraping-code.py
@@ -2,79 +2,130 @@ import requests
 from bs4 import BeautifulSoup
 import re
 import numpy as np
-from time import sleep
+from time import sleep, time
 import pandas as pd
 
-zonas = ['norte', 'sul', 'leste', 'oeste']
-paginas = ['', '_Desde_49', '_Desde_97', '_Desde_145', '_Desde_193', '_Desde_241', '_Desde_289']
-url_ml = 'https://imoveis.mercadolivre.com.br/casas/aluguel/sao-paulo/sao-paulo-zona-{}/{}'
 
-re_precos = r'<span class="price-tag-fraction">(.*)<'
-re_quartos = r'([1-9]{1,2}) quarto[s]?'
-re_areas = r'<li class="ui-search-card-attributes__attribute">(.*) m²'
+paginas = ['', '_Desde_49', '_Desde_97', '_Desde_145',
+           '_Desde_193', '_Desde_241', '_Desde_289']
 
 
-class Scraper:
-    zonas = []
-    areas = []
-    quartos = []
-    precos = []
+def create_links(quantidade):
+    zonas = ['norte', 'sul', 'leste', 'oeste']
+    link = 'https://imoveis.mercadolivre.com.br/casas/aluguel/sao-paulo/sao-paulo-zona-{}/{}'
+
+    for zona in zonas:
+        cont = 0
+        var = 49
+
+        while cont < quantidade:
+            if cont == 0:
+                num_pag = ''
+
+            else:
+                num_pag = f'_Desde_{var}'
+                var += 48
+
+            yield link.format(zona, num_pag)
+            cont += 1
+
+
+def get_html(link):
+    c = requests.get(link).content
+    soup = BeautifulSoup(c, 'html.parser')
+
+    return soup
+
+
+def get_precos(soup_html):
+    re_precos = r'<span class="price-tag-fraction">(.*)<'
+    lista_precos = []
+
+    for dado in soup_html.select('.ui-search-result__content-wrapper'):
+        preco = dado.select_one('.price-tag-fraction')
+        precos = re.findall(re_precos, str(preco))
+
+        if len(precos) == 0:
+            lista_precos.append(np.nan)
+        else:
+            p = int(precos[0].replace('.', ''))
+            lista_precos.append(p)
+
+    return lista_precos
+
+
+def get_atributos(soup_html):
+
+    re_quartos = r'([1-9]{1,2}) quarto[s]?'
+    re_areas = r'<li class="ui-search-card-attributes__attribute">(.*) m²'
+
+    lista_areas = []
+    lista_quartos = []
+
+    for dado in soup_html.select('.ui-search-result__content-wrapper'):
+        metro = dado.select_one('.ui-search-card-attributes__attribute ')
+        metros = re.findall(re_areas, str(metro))
+
+        quartos = re.findall(re_quartos, dado.text)
+
+        if len(metros) == 0:
+            lista_areas.append(np.nan)
+        else:
+            lista_areas.append(int(metros[0]))
+
+        if len(quartos) == 0:
+            lista_quartos.append(np.nan)
+        else:
+            q = int(quartos[0])
+            lista_quartos.append(q)
+
+    return lista_areas, lista_quartos
+
+
+def cria_dataframe(lista_precos, lista_atributos):
     dados = {}
 
-    def __init__(self, url, zona, pagina):
+    dados['preco'] = lista_precos
+    dados['area'] = lista_atributos[0]
+    dados['quartos'] = lista_atributos[1]
 
-        self.zona = zona
-        self.url = url.format(zona, pagina)
+    df = pd.DataFrame(dados)
 
-    def get_atributes(self):
+    return df
 
-        c = requests.get(self.url).content
-        soup = BeautifulSoup(c, 'html.parser')
 
-        for dado in soup.select('.ui-search-result__content-wrapper'):
-            preco = dado.select_one('.price-tag-fraction')
-            metros = dado.select_one('.ui-search-card-attributes__attribute ')
-
-            precos = re.findall(re_precos, str(preco))
-            areas = re.findall(re_areas, str(metros))
-            quartos = re.findall(re_quartos, dado.text)
-
-            for p in precos:
-                if isinstance(p, str):
-                    p = int(p.replace('.', '').replace(',', '').strip())
-                    self.precos.append(p)
-
-            if len(areas) == 0:
-                areas.append(np.nan)
-            for a in areas:
-                if isinstance(a, str):
-                    a = int(a.replace('.', '').replace(',', '').strip())
-                self.areas.append(a)
-
-            if len(quartos) == 0:
-                quartos.append(np.nan)
-            for q in quartos:
-                self.quartos.append(q)
-
-            self.zonas.append(self.zona)
-
-            self.dados['zona'] = self.zonas
-            self.dados['quartos'] = self.quartos
-            self.dados['area'] = self.areas
-            self.dados['preco'] = self.precos
-
-        sleep(2)
-        return self.dados
-
-    def create_csv(self):
-        df = pd.DataFrame(self.dados)
-        df.to_csv('dados_imoveis.csv', index=False)
+def create_csv(self):
+    df = pd.DataFrame(self.dados)
+    df.to_csv('dados_imoveis.csv', index=False)
 
 
 if __name__ == '__main__':
-    for zona in zonas:
-        for pagina in paginas:
-            sleep(2)
-            scrap = Scraper(url_ml, zona, pagina)
-            scrap.get_atributes()
-            scrap.create_csv()
+    t1 = time()
+    gen = create_links(2)
+
+    for i in gen:
+        print(i)
+
+    """html = get_html(
+        'https://imoveis.mercadolivre.com.br/casas/aluguel/sao-paulo/sao-paulo-zona-sul/_Desde_49')
+    
+    print(f'get_html: {time() - t1} segundos')
+    print()
+    
+    t2 = time()
+    prec = get_precos(html)
+    print(f'get_precos: {time() - t2} segundos')
+    print()
+
+    t3 = time()
+    atributos = get_atributos(html)
+    print(f'get_atributos: {time() - t3} segundos')
+    print()
+
+    t4 = time()
+    print(cria_dataframe(prec, atributos))
+    print(f'cria_dataframe: {time() - t4} segundos')
+
+    print()
+    print(f'Tempo total: {time() - t1}')
+"""
